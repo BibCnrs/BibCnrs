124a8a3db94fa511a560bdeac82be1bc598df08a
Jonathan ARNAULT <jonathan@marmelab.com>
2024-05-21 11:53:00 +0200

    Feat(api): Add proxy to preprod

--- packages/bib-api/src/main.ts
+++ packages/bib-api/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from "@nestjs/core";
+import { proxy } from "packages/bib-api/src/proxy";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+	app.use(proxy);
 	await app.listen(3000);
 }
 bootstrap();
