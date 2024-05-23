22b72cf5c11eb2752ef1948a1d548b51c644c479
Jonathan ARNAULT <jonathan@marmelab.com>
2024-05-23 14:04:09 +0200

    Fix types

--- packages/bib-api/src/admin/admin-users/admin-users.service.spec.ts
+++ packages/bib-api/src/admin/admin-users/admin-users.service.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from "@nestjs/testing";
+import { beforeEach, describe, expect, it } from "vitest";
 import { PrismaModule } from "../../prisma/prisma.module";
 import { AdminUsersService } from "./admin-users.service";
 
