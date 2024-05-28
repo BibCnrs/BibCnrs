4a6425c64a5196744c9bea7c608e7302066c11ba
Jonathan ARNAULT <jonathan@marmelab.com>
2024-05-28 11:02:50 +0200

    Fix(ops): Revert removed proxy

--- packages/bib-api/src/main.ts
+++ packages/bib-api/src/main.ts
@@ -2,10 +2,12 @@ import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import cookieParser from "cookie-parser";
 import { AppModule } from "./app.module";
+import { NotFoundFilter } from "./proxy/not-found.filter";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.use(cookieParser());
+	app.useGlobalFilters(new NotFoundFilter());
 	app.useGlobalPipes(
 		new ValidationPipe({
 			transform: true,
---
+++ packages/bib-api/src/proxy/not-found.filter.ts
@@ -0,0 +1,29 @@
+import {
+	ArgumentsHost,
+	Catch,
+	ExceptionFilter,
+	HttpException,
+} from "@nestjs/common";
+import { Request, Response } from "express";
+import { proxy } from "./proxy.middleware";
+
+@Catch(HttpException)
+export class NotFoundFilter implements ExceptionFilter {
+	catch(exception: HttpException, host: ArgumentsHost) {
+		const ctx = host.switchToHttp();
+		const response = ctx.getResponse<Response>();
+		const request = ctx.getRequest<Request>();
+		const status = exception.getStatus();
+
+		if (status === 404) {
+			proxy(request, response);
+			return;
+		}
+
+		response.status(status).json({
+			statusCode: status,
+			timestamp: new Date().toISOString(),
+			path: request.url,
+		});
+	}
+}
