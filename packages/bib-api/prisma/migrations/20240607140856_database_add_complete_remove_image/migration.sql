99ed2d77c63e49bd005826d300eebe06b8cb306b
Jonathan ARNAULT <jonathan@marmelab.com>
2024-06-07 16:43:02 +0200

    Fix tests

--- packages/bib-api/prisma/migrations/20240607140856_database_add_complete_remove_image/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `image` on the `database` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "database" DROP COLUMN "image",
-ADD COLUMN     "complete" BOOLEAN NOT NULL DEFAULT false;
