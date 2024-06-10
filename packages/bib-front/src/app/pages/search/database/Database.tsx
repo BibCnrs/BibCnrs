175e0c8bd58b2bfb076f3f69ce4466d4cff765ca
Anthony Rimet <anthony.rimet03@gmail.com>
2024-06-10 17:02:36 +0200

    Feat(database): WIP database design

--- packages/bib-front/src/app/pages/search/database/Database.tsx
+++ packages/bib-front/src/app/pages/search/database/Database.tsx
@@ -3,6 +3,7 @@ import Switch from "@mui/material/Switch";
 import { keepPreviousData, useQuery } from "@tanstack/react-query";
 import {
 	type ChangeEvent,
+	useCallback,
 	useContext,
 	useEffect,
 	useMemo,
@@ -23,11 +24,15 @@ import { useLanguageKey, useTranslator } from "../../../shared/locales/I18N";
 import type {
 	DatabaseDataType,
 	DatabaseItemProps,
+	TypeDatabaseEnum,
 } from "../../../shared/types/data.types";
 import "./Database.scss";
 import {
 	Box,
+	Checkbox,
 	CircularProgress,
+	FormGroup,
+	FormLabel,
 	MenuItem,
 	Pagination,
 	Select,
@@ -36,10 +41,72 @@ import {
 import { Stack } from "@mui/system";
 import { DatabaseItem } from "./DatabaseItem";
 
+const INITIAL_FILTER = [
+	{ props: "oa", value: false, type: "boolean", section: "default" },
+	{
+		props: "is_text_integral",
+		value: false,
+		type: "boolean",
+		section: "default",
+	},
+	{
+		props: "is_completed",
+		value: false,
+		type: "boolean",
+		section: "default",
+	},
+	{
+		props: "without_embargo",
+		value: false,
+		type: "boolean",
+		section: "default",
+	},
+	{
+		props: "active",
+		value: false,
+		type: "boolean",
+		section: "document",
+	},
+	{
+		props: "is_archived",
+		value: false,
+		type: "boolean",
+		section: "document",
+	},
+	{
+		props: "type",
+		target: "news",
+		value: false,
+		type: "boolean",
+		section: "content",
+	},
+	{
+		props: "type",
+		target: "book",
+		value: false,
+		type: "boolean",
+		section: "content",
+	},
+	{
+		props: "type",
+		target: "database",
+		value: false,
+		type: "boolean",
+		section: "content",
+	},
+	{
+		props: "type",
+		target: "data",
+		value: false,
+		type: "boolean",
+		section: "content",
+	},
+];
+
 const Database = () => {
 	const { login, theme, search } = useContext(BibContext);
 	const serviceCatch = useServicesCatch();
-	const [oa, setOa] = useState(!login);
+	const [filters, setFilters] = useState(INITIAL_FILTER);
 	const t = useTranslator();
 	const language = useLanguageKey();
 
@@ -59,8 +126,8 @@ const Database = () => {
 		// biome-ignore lint/suspicious/noExplicitAny: Need to type after marmelab's mission
 		any
 	>({
-		queryKey: ["database", oa, search.domain],
-		queryFn: () => database(language, oa, search.domain),
+		queryKey: ["database", search.domain],
+		queryFn: () => database(language, search.domain),
 		placeholderData: keepPreviousData,
 		staleTime: 3600000, // 1 hour of cache
 		gcTime: 3600000, // 1000 * 60 * 60
@@ -90,14 +157,26 @@ const Database = () => {
 		[databases, databasePerPage],
 	);
 
-	const pagedDatabases = useMemo(
-		() =>
-			databases.slice(
-				(currentPage - 1) * databasePerPage,
-				currentPage * databasePerPage,
-			),
-		[currentPage, databasePerPage, databases],
-	);
+	const filteredDatabases = useMemo(() => {
+		return databases.filter((result) => {
+			return filters.every((filter) => {
+				if (!filter.value) return true;
+				if (filter.props === "type") {
+					return result[filter.props].includes(
+						filter.target as TypeDatabaseEnum,
+					);
+				}
+				return result[filter.props] === filter.value;
+			});
+		});
+	}, [databases, filters]);
+
+	const pagedDatabases = useMemo(() => {
+		return filteredDatabases.slice(
+			(currentPage - 1) * databasePerPage,
+			currentPage * databasePerPage,
+		);
+	}, [currentPage, databasePerPage, filteredDatabases]);
 
 	useEffect(() => {
 		setCurrentPage(Math.max(1, Math.min(currentPage, pageCount)));
@@ -108,10 +187,6 @@ const Database = () => {
 			serviceCatch(error);
 		}
 	}, [error, isError, serviceCatch]);
-
-	useEffect(() => {
-		setOa(!login);
-	}, [login]);
 
 	const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
 		setNameFilter(e.target.value.toLocaleUpperCase());
@@ -130,23 +205,7 @@ const Database = () => {
 				</div>
 			</div>
 			<Stack id="app" gap={2}>
-				{login ? (
-					<FormControlLabel
-						id="database-oa"
-						className="text"
-						control={
-							<Switch
-								value={oa}
-								size="small"
-								onClick={() => {
-									setOa(!oa);
-								}}
-							/>
-						}
-						label={t("pages.database.oa")}
-						labelPlacement="end"
-					/>
-				) : (
+				{!login && (
 					<ColoredPaper
 						id="database-anonymous"
 						elevation={4}
@@ -157,58 +216,59 @@ const Database = () => {
 					</ColoredPaper>
 				)}
 
-				<Stack direction="row" spacing={2}>
-					<TextField
-						fullWidth
-						label={t("pages.database.searchDatabase")}
-						value={nameFilter}
-						onChange={handleSearchChange}
-						autoComplete="off"
-					/>
-					<Select
-						value={databasePerPage}
-						onChange={(event) =>
-							setDatabasePerPage(event.target.value as number)
-						}
-					>
-						<MenuItem value={10}>10</MenuItem>
-						<MenuItem value={25}>25</MenuItem>
-						<MenuItem value={50}>50</MenuItem>
-					</Select>
-				</Stack>
-
-				{isLoading ? (
-					<Stack alignItems="center" spacing={5}>
-						<CircularProgress />
+				<Box
+					display="grid"
+					gridTemplateColumns={{
+						xs: "1fr 4fr",
+					}}
+					gap={3}
+				>
+					<Box>
+						<FilterTab
+							setFilters={setFilters}
+							filters={filters}
+							databases={filteredDatabases}
+						/>
+					</Box>
+					<Stack gap={2}>
+						<TextField
+							label={t("pages.database.searchDatabase")}
+							value={nameFilter}
+							onChange={handleSearchChange}
+							autoComplete="off"
+							sx={{ width: "40%" }}
+						/>
+						{isLoading ? (
+							<Stack alignItems="center" spacing={5}>
+								<CircularProgress />
+							</Stack>
+						) : (
+							<>
+								<Box
+									display="grid"
+									gridTemplateColumns={{
+										xs: "repeat(1, 1fr)",
+										sm: "repeat(2, 1fr)",
+										md: "repeat(3, 1fr)",
+										lg: "repeat(4, 1fr)",
+									}}
+									gap={2}
+								>
+									{pagedDatabases.map((item) => (
+										<DatabaseItem key={item.id} {...item} />
+									))}
+								</Box>
+								<DatabasePagination
+									pageCount={pageCount}
+									currentPage={currentPage}
+									setCurrentPage={setCurrentPage}
+									databasePerPage={databasePerPage}
+									setDatabasePerPage={setDatabasePerPage}
+								/>
+							</>
+						)}
 					</Stack>
-				) : (
-					<>
-						<DatabasePagination
-							pageCount={pageCount}
-							currentPage={currentPage}
-							setCurrentPage={setCurrentPage}
-						/>
-						<Box
-							display="grid"
-							gridTemplateColumns={{
-								xs: "repeat(1, 1fr)",
-								sm: "repeat(2, 1fr)",
-								md: "repeat(3, 1fr)",
-								lg: "repeat(4, 1fr)",
-							}}
-							gap={2}
-						>
-							{pagedDatabases.map((item) => (
-								<DatabaseItem key={item.id} {...item} />
-							))}
-						</Box>
-						<DatabasePagination
-							pageCount={pageCount}
-							currentPage={currentPage}
-							setCurrentPage={setCurrentPage}
-						/>
-					</>
-				)}
+				</Box>
 			</Stack>
 		</div>
 	);
@@ -218,20 +278,128 @@ function DatabasePagination({
 	pageCount,
 	currentPage,
 	setCurrentPage,
+	databasePerPage,
+	setDatabasePerPage,
 }: {
 	pageCount: number;
 	currentPage: number;
 	setCurrentPage: (value: number) => void;
+	databasePerPage: number;
+	setDatabasePerPage: (value: number) => void;
 }) {
 	return (
-		<Stack spacing={2} alignItems="flex-end">
+		<Stack
+			spacing={2}
+			gap={2}
+			alignItems="flex-end"
+			justifyContent="flex-end"
+			flexDirection="row"
+		>
+			<Select
+				value={databasePerPage}
+				onChange={(event) => setDatabasePerPage(event.target.value as number)}
+				size="small"
+				label="Database per page"
+				variant="standard"
+			>
+				<MenuItem value={10}>10</MenuItem>
+				<MenuItem value={25}>25</MenuItem>
+				<MenuItem value={50}>50</MenuItem>
+			</Select>
 			<Pagination
 				count={pageCount}
 				color="primary"
 				page={currentPage}
 				onChange={(_, value) => setCurrentPage(value)}
+				size="small"
 			/>
 		</Stack>
+	);
+}
+
+function FilterTab({
+	setFilters,
+	filters,
+	databases,
+}: {
+	setFilters: (value) => void;
+	filters: typeof INITIAL_FILTER;
+	databases: DatabaseItemProps[];
+}) {
+	const handleChange = (filter) => {
+		setFilters((prev) => {
+			const newFilters = prev.map((item) => {
+				if (item.props === filter.props) {
+					if (item.target && filter.target && item.target === filter.target) {
+						return {
+							...item,
+							value: !item.value,
+						};
+					}
+					if (!item.target && !filter.target) {
+						return {
+							...item,
+							value: !item.value,
+						};
+					}
+				}
+				return item;
+			});
+
+			return newFilters;
+		});
+	};
+
+	const sections = [...new Set(filters.map((item) => item.section))];
+
+	// for each filter, count the number of databases that have this filter and set in key for each filter
+	const countsByFilter = [];
+	for (let i = 0; i < filters.length; i++) {
+		const filter = filters[i];
+		const count = databases.filter((value) => {
+			if (filter.props === "type") {
+				return value[filter.props].includes(filter.target as TypeDatabaseEnum);
+			}
+			return value[filter.props] === true;
+		}).length;
+
+		const key =
+			filter.props === "type"
+				? `${filter.props}_${filter.target}`
+				: filter.props;
+		countsByFilter[key] = count;
+	}
+
+	console.log("countsByFilter", countsByFilter);
+
+	return (
+		<FormGroup>
+			{sections.map((section) => (
+				<Stack key={section}>
+					<FormLabel component="legend">{section}</FormLabel>
+					{filters
+						.filter((item) => item.section === section)
+						.map((filter, index) => (
+							<FormControlLabel
+								key={`${filter.props}-${index}`}
+								control={
+									<Checkbox
+										checked={filter.value}
+										onChange={() => handleChange(filter)}
+									/>
+								}
+								label={`${filter.props} (${
+									countsByFilter[
+										filter.props === "type"
+											? `${filter.props}_${filter.target}`
+											: filter.props
+									]
+								})`}
+							/>
+						))}
+				</Stack>
+			))}
+		</FormGroup>
 	);
 }
 
--- packages/bib-front/src/app/pages/search/database/DatabaseItem.tsx
+++ packages/bib-front/src/app/pages/search/database/DatabaseItem.tsx
@@ -1,11 +1,4 @@
-import {
-	Card,
-	CardContent,
-	Link,
-	Stack,
-	Tooltip,
-	Typography,
-} from "@mui/material";
+import { Card, CardContent, Link, Stack, Tooltip } from "@mui/material";
 import DatabaseIcons from "../../../components/element/icon/DatabaseIcons";
 import type { DatabaseItemProps } from "../../../shared/types/data.types";
 
@@ -13,7 +6,13 @@ export function DatabaseItem(props: DatabaseItemProps) {
 	return (
 		<Tooltip title={props.text} arrow>
 			<Card>
-				<CardContent>
+				<CardContent
+					sx={{
+						"&:last-child": {
+							paddingBottom: 2,
+						},
+					}}
+				>
 					<Stack gap={1}>
 						<Link fontWeight={700} href={props.url}>
 							{props.name}
