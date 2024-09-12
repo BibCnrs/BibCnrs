e3217533e3784dacad2f02757e8c8966c47033e1
Jonathan ARNAULT <jonathan@marmelab.com>
2024-09-12 13:39:26 +0200

    Fix(migrations): Remove drop index

--- packages/api/prisma/migrations/20240911085630_janus_account_add_platform_view/migration.sql
+++ packages/api/prisma/migrations/20240911085630_janus_account_add_platform_view/migration.sql
@@ -1,8 +1,5 @@
 -- CreateEnum
 CREATE TYPE "PlatformView" AS ENUM ('card', 'list');
 
--- DropIndex
-DROP INDEX "unique_database_name";
-
 -- AlterTable
 ALTER TABLE "janus_account" ADD COLUMN     "platformView" "PlatformView" NOT NULL DEFAULT 'card';
