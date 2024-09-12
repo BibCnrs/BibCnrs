23422d68813b604840c4182e1cd20487df0500a6
Jonathan ARNAULT <jonathan@marmelab.com>
2024-09-12 13:45:22 +0200

    Fix(ops): add condition to index drop

--- packages/api/prisma/migrations/20240911085630_janus_account_add_platform_view/migration.sql
+++ packages/api/prisma/migrations/20240911085630_janus_account_add_platform_view/migration.sql
@@ -1,5 +1,8 @@
 -- CreateEnum
 CREATE TYPE "PlatformView" AS ENUM ('card', 'list');
 
+-- DropIndex
+DROP INDEX IF EXISTS "unique_database_name";
+
 -- AlterTable
 ALTER TABLE "janus_account" ADD COLUMN     "platformView" "PlatformView" NOT NULL DEFAULT 'card';
