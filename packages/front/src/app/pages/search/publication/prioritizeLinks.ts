3275e7971ecae7656e4996f5673332cd4e682fb8
Jonathan ARNAULT <jonathan@marmelab.com>
2024-09-13 10:40:11 +0200

    Fix(publication): Rollback priority algorithm

--- packages/front/src/app/pages/search/publication/prioritizeLinks.ts
+++ packages/front/src/app/pages/search/publication/prioritizeLinks.ts
@@ -1,7 +1,3 @@
 import { getPrioritizedLink_v1 } from "./prioritizeLinksV1";
-import { getPrioritizedLink_v2 } from "./prioritizeLinksV2";
 
-export const getPrioritizedLink =
-	import.meta.env.ENABLE_V1_LINK_PRIORIZATION === "y"
-		? getPrioritizedLink_v1
-		: getPrioritizedLink_v2;
+export const getPrioritizedLink = getPrioritizedLink_v1;
