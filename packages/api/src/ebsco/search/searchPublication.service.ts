2a42d8fe96d3a22ba314aee611ea367cda0016bf
ramy-amrani <ramy.amrani@inist.fr>
2025-08-27 13:59:15 +0200

    fix(publications): fix lint

--- packages/api/src/ebsco/search/searchPublication.service.ts
+++ packages/api/src/ebsco/search/searchPublication.service.ts
@@ -156,7 +156,9 @@ export class EbscoSearchPublicationService extends AbstractEbscoSearchService {
 						const formatedIssn = `${formatISSN(item.issnOnline[0])}`;
 						if (apcMap.has(formatedIssn)) {
 							item.isDiamond = apcMap.get(formatedIssn).has_apc === false;
-							item.isS2O = apcMap.get(formatedIssn).has_apc === false || apcMap.get(formatedIssn).labels.includes("s2o");
+							item.isS2O =
+								apcMap.get(formatedIssn).has_apc === false ||
+								apcMap.get(formatedIssn).labels.includes("s2o");
 						}
 					}
 				} catch {
